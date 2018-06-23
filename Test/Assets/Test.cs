9cec781f087ac9276e2db5d3bf8c8f6f72f6139f
sacopon <sacopon.dev@gmail.com>
2018-06-23 22:18:31 +0900

    p023
    スクリプトの Update() メソッドに処理を記述し、和尚を動かす.

--- Test/Assets/Test.cs
+++ Test/Assets/Test.cs
@@ -11,6 +11,10 @@ public class Test : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+		Transform tr = GetComponent<Transform>();
+		Vector3 pos = new Vector3(
+			Random.Range(-2.0f, 2.0f),
+			Random.Range(-2.0f, 2.0f));
+		tr.position = pos;
 	}
 }
