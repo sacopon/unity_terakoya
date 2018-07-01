6cf343f0c01b45c96790086056824ad9f8282d4b
sacopon <sacopon.dev@gmail.com>
2018-07-01 23:16:53 +0900

    p057.
    GameManager.cs 変数定義追加.

--- TheTemple/Assets/Scripts/GameManager.cs
+++ TheTemple/Assets/Scripts/GameManager.cs
@@ -3,6 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour {
+	// 定数定義
+	private const int MAX_ORB = 10;	// オーブ最大数
+
+	// オブジェクト参照
+	public GameObject orbPrefab;	// オーブプレハブ
+	public GameObject canvasGame;	// ゲームキャンバス
 
 	// Use this for initialization
 	void Start () {
