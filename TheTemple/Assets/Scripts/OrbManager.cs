b49130bae720f67566182abe899e50bad923c033
sacopon <sacopon.dev@gmail.com>
2018-07-10 23:00:43 +0900

    p67.
    オーブの削除

--- TheTemple/Assets/Scripts/OrbManager.cs
+++ TheTemple/Assets/Scripts/OrbManager.cs
@@ -4,9 +4,12 @@ using UnityEngine;
 
 public class OrbManager : MonoBehaviour {
 
+	// オブジェクト参照
+	private GameObject gameManager;	// ゲームマネージャ
+
 	// Use this for initialization
 	void Start () {
-		
+		gameManager = GameObject.Find("GameManager");
 	}
 	
 	// Update is called once per frame
@@ -16,5 +19,11 @@ public class OrbManager : MonoBehaviour {
 
 	// オーブ取得
 	public void TouchOrb() {
+		if (Input.GetMouseButton(0) == false) {
+			return;
+		}
+
+		gameManager.GetComponent<GameManager>().GetOrb();
+		Destroy(this.gameObject);
 	}
 }
