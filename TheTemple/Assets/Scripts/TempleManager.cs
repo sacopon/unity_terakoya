6638ecf80611d231699ef63ac1066af9474b30ca
sacopon <sacopon.dev@gmail.com>
2018-07-11 21:49:41 +0900

    p076 寺の絵を設定するメソッドと大きさを設定するメソッドを実装.

--- TheTemple/Assets/Scripts/TempleManager.cs
+++ TheTemple/Assets/Scripts/TempleManager.cs
@@ -2,7 +2,11 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+using UnityEngine.UI;
+
 public class TempleManager : MonoBehaviour {
+
+	public Sprite[] templePicture = new Sprite[3];	// 寺の絵
 
 	// Use this for initialization
 	void Start () {
@@ -12,5 +16,16 @@ public class TempleManager : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		
+	}
+
+	// 寺の絵の設定
+	public void SetTemplePicture(int level) {
+		GetComponent<Image> ().sprite = templePicture [level];
+	}
+
+	// 寺の大きさ設定
+	public void SetTempleScale (int score, int nextScore) {
+		float scale = 0.5f + (((float)score / (float)nextScore) / 2.0f);
+		transform.localScale = new Vector3 (scale, scale, 1.0f);
 	}
 }
