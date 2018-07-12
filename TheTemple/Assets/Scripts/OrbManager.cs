c6ad06d5d74bb47b75fda949e0a23253af5d0dae
sacopon <sacopon.dev@gmail.com>
2018-07-12 23:23:15 +0900

    p084 オーブの種類を設定できる処理を実装.

--- TheTemple/Assets/Scripts/OrbManager.cs
+++ TheTemple/Assets/Scripts/OrbManager.cs
@@ -2,10 +2,22 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+using UnityEngine.UI;
+
 public class OrbManager : MonoBehaviour {
 
 	// オブジェクト参照
 	private GameObject gameManager;	// ゲームマネージャ
+
+	public Sprite[] orbPicture = new Sprite[3];	// オーブの絵
+
+	public enum ORB_KIND {	// オーブの種類を定義
+		BLUE,
+		GREEN,
+		PURPLE,
+	}
+
+	private ORB_KIND orbKind;	// オーブの種類
 
 	// Use this for initialization
 	void Start () {
@@ -23,7 +35,40 @@ public class OrbManager : MonoBehaviour {
 			return;
 		}
 
-		gameManager.GetComponent<GameManager>().GetOrb();
+		switch (orbKind) {
+			case ORB_KIND.BLUE:
+				gameManager.GetComponent<GameManager>().GetOrb (1);
+				break;
+
+			case ORB_KIND.GREEN:
+				gameManager.GetComponent<GameManager>().GetOrb (5);
+				break;
+
+			case ORB_KIND.PURPLE:
+				gameManager.GetComponent<GameManager>().GetOrb (10);
+				break;
+		}
+
+
 		Destroy(this.gameObject);
+	}
+
+	// オーブの種類を設定
+	public void SetKind (ORB_KIND kind) {
+		orbKind = kind;
+
+		switch (orbKind) {
+			case ORB_KIND.BLUE:
+				GetComponent<Image> ().sprite = orbPicture [0];
+				break;
+
+			case ORB_KIND.GREEN:
+				GetComponent<Image> ().sprite = orbPicture [1];
+				break;
+
+			case ORB_KIND.PURPLE:
+				GetComponent<Image> ().sprite = orbPicture [2];
+				break;
+		}
 	}
 }
