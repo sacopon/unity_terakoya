d61143a6006ed27e9c85e2c7a8efc4cc8ae7e587
sacopon <sacopon.dev@gmail.com>
2018-07-16 17:03:22 +0900

    p136 オーブ取得時にオーブを移動/スケールアニメーションさせる.

--- TheTemple/Assets/Scripts/OrbManager.cs
+++ TheTemple/Assets/Scripts/OrbManager.cs
@@ -35,6 +35,25 @@ public class OrbManager : MonoBehaviour {
 		if (Input.GetMouseButton(0) == false) {
 			return;
 		}
+
+		RectTransform rect = GetComponent<RectTransform>();
+
+		// オーブの奇跡設定
+		Vector3[] path = {
+			new Vector3(rect.localPosition.x * 1.5f, 300f, 0f),
+			new Vector3(0f, 150f, 0f),
+		};
+
+		// DOTween を使ったアニメ生成
+		rect.DOLocalPath(path, 0.5f, PathType.CatmullRom)
+			.SetEase(Ease.OutQuad)
+			.OnComplete(AddOrbPoint);
+
+		// 同時にサイズも変更
+		rect.DOScale(
+			new Vector3(0.5f, 0.5f, 0f),
+			0.5f
+		);
 	}
 
 	// オーブアニメ終了後にポイント加算処理をする
